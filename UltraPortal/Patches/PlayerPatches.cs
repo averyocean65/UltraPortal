3eff87c4950e26bc30c2f3cb364e0002c0c96498
averyocean65 <averytoad65@gmail.com>
2026-03-04 00:24:35 +0100

    remove redundant comment

--- UltraPortal/Patches/PlayerPatches.cs
+++ UltraPortal/Patches/PlayerPatches.cs
@@ -7,17 +7,6 @@ namespace UltraPortal {
 		[HarmonyPrefix]
 		[HarmonyPatch(typeof(NewMovement), nameof(NewMovement.Respawn))]
 		static void RespawnPatch() {
-			// void ToggleObject(MonoBehaviour behaviour) {
-			// 	if (!behaviour) {
-			// 		return;
-			// 	}
-			// 	
-			// 	bool prevState = behaviour.gameObject.activeSelf;
-			// 	behaviour.gameObject.SetActive(true);
-			// 	behaviour.Invoke("Reset", 0);
-			// 	behaviour.gameObject.SetActive(prevState);
-			// }
-			
 			PortalGun portalGun = GameObject.FindObjectOfType<PortalGun>(true);
 			if(portalGun)
 				portalGun.Reset();
