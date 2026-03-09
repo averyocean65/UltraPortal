388b43774af02f3323e27219cb37f3f3ecaa3e41
averyocean65 <averytoad65@gmail.com>
2026-03-09 12:26:06 +0100

    fix config types

--- UltraPortal/ModConfig.cs
+++ UltraPortal/ModConfig.cs
@@ -9,7 +9,7 @@ namespace UltraPortal {
 		// note: i do not have a better name for this
 		
 		[Configgable("Gameplay")]
-		public static ConfigInputField<bool> UseOtherPortalForProjectileTeleport = new ConfigInputField<bool>(true);
+		public static ConfigToggle UseOtherPortalForProjectileTeleport = new ConfigToggle(true);
 
 		[Configgable("Visuals")]
 		public static ConfigColor PrimaryPortalColor = new ConfigColor(new Color(0.9882352941f, 0.0117647059f, 0.2705882353f));
@@ -30,10 +30,10 @@ namespace UltraPortal {
 		public static IntegerSlider ExplosionEmissionIntensity = new IntegerSlider(-1, -5, 5);
 		
 		[Configgable("Visuals")]
-		public static ConfigInputField<bool> UseEmission = new ConfigInputField<bool>(true);
+		public static ConfigToggle UseEmission = new ConfigToggle(true);
 		
 		[Configgable("Visuals")]
-		public static ConfigInputField<bool> ShowPortalSpawnParticles = new ConfigInputField<bool>(true);
+		public static ConfigToggle ShowPortalSpawnParticles = new ConfigToggle(true);
 		
 		[Configgable("Visuals/Style")]
 		public static ConfigColor SafetyHazardColor = new ConfigColor(new Color(1, 0, 0));
@@ -42,10 +42,10 @@ namespace UltraPortal {
 		public static ConfigInputField<float> PortalProjectileSpeed = new ConfigInputField<float>(95.0f);
 
 		[Configgable("Gameplay/Explosions")]
-		public static ConfigInputField<bool> AreExplosionsUltraboosters = new ConfigInputField<bool>(false);
+		public static ConfigToggle AreExplosionsUltraboosters = new ConfigToggle(false);
 
 		[Configgable(displayName: "Enabled")]
-		public static ConfigInputField<bool> IsEnabled = new ConfigInputField<bool>(true);
+		public static ConfigToggle IsEnabled = new ConfigToggle(true);
 
 		// note: i also do not have a better name for these
 		
