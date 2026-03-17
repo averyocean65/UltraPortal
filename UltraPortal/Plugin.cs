3f8287dcfef27e357f62c98825a691a032b49cf1
averyocean65 <averytoad65@gmail.com>
2026-03-17 18:24:03 +0100

    update bepinex plugin info

--- UltraPortal/Plugin.cs
+++ UltraPortal/Plugin.cs
@@ -22,7 +22,7 @@ namespace UltraPortal {
         private static class PluginInfo {
             public const string Name = "ULTRAPORTAL";
             public const string Guid = "com.ultraportal";
-            public const string Version = "0.1.1";
+            public const string Version = "0.2.0";
         }
         
         public static ManualLogSource LogSource { get; private set; }
